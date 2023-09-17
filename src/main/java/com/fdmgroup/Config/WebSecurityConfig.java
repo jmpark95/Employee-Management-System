d0ec71e4a161a9c11daebf7dc1c44397edb0604a
jmpark95 <jmpark95@live.com>
2023-09-18 00:04:29 +1000

    Added CORS for new azure domain

--- src/main/java/com/fdmgroup/Config/WebSecurityConfig.java
+++ src/main/java/com/fdmgroup/Config/WebSecurityConfig.java
@@ -85,7 +85,7 @@ public class WebSecurityConfig {
 		CorsConfiguration configuration = new CorsConfiguration();
 		UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
 
-		configuration.setAllowedOrigins(Arrays.asList("http://localhost:5173", "https://minpark-ems.netlify.app"));
+		configuration.setAllowedOrigins(Arrays.asList("http://localhost:5173", "https://minpark-ems.netlify.app", "https://red-bush-017446800.3.azurestaticapps.net"));
 		configuration.setAllowedMethods(Arrays.asList("*"));
 		configuration.setAllowedHeaders(Arrays.asList("*"));
 		configuration.setExposedHeaders(Arrays.asList("Authorization"));
